a8d1c871b6731f3f4227f48f3a799be9446eeb33
fabiodplacerda <lacerda96@gmail.com>
2024-01-25 16:34:29 +0000

    changed some styles and added media queries

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'reactjs-popup/dist/index.css';
 import './index.css';
+import './mediaQueries.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
